18a0a67cf191d559c0ad0b4baf06651fc4350b20
hibikir <hibikir@gmail.com>
2014-11-28 13:27:23 -0600

    Better output with more information

--- src/test/scala/Montecarlo.scala
+++ src/test/scala/Montecarlo.scala
@@ -6,15 +6,17 @@ class Montecarlo extends FunSpec{
   describe("Testing ideal Shoot The Moon conditions") {
     it("count how many tricks are face down per player"){
       val tricks  = 0.to(100000).par.flatMap { x =>
-        val results = new Game(players.take(3)).play
+        val results = new Game(players.take(4)).play
         //assert(results.size == 4)
         //assert(results.exists(_.hand.size == 0))
         //assert((results.map(_.hand.size).sum - 2) % 3 == 0)
-        results.map(_.tricks.size)
+        results.map(x=>(x.tricks.size,x.hand.size))
       }
 
-      val tMap = tricks.seq.groupBy(x=>x)
-      println(tMap.mapValues(x=>x.size).toSeq.sortBy(_._1))
+      val tMap = tricks.seq.groupBy(x=>x._1)
+      tMap.mapValues(x=>(x.size,x.map(_._2).sum)).toSeq.sortBy(_._1).foreach(x=>
+        println(x._1 + "\t" +x._2._1 +"\t" + x._2._2)
+      )
     }
   }
 }
