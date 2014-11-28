4f755ab6265d19fd64db1034a7a913902c18e73d
hibikir <hibikir@gmail.com>
2014-11-28 11:04:41 -0600

    Parallelized the long running test

--- src/test/scala/Montecarlo.scala
+++ src/test/scala/Montecarlo.scala
@@ -5,7 +5,7 @@ class Montecarlo extends FunSpec{
   val players = 0.to(10).map(x=>Player(x.toString,x))
   describe("Testing ideal Shoot The Moon conditions") {
     it("count how many tricks are face down per player"){
-      val tricks  = 0.to(100000).flatMap { x =>
+      val tricks  = 0.to(100000).par.flatMap { x =>
         val results = new Game(players.take(3)).play
         //assert(results.size == 4)
         //assert(results.exists(_.hand.size == 0))
@@ -13,7 +13,7 @@ class Montecarlo extends FunSpec{
         results.map(_.tricks.size)
       }
 
-      val tMap = tricks.groupBy(x=>x)
+      val tMap = tricks.seq.groupBy(x=>x)
       println(tMap.mapValues(x=>x.size).toSeq.sortBy(_._1))
     }
   }
